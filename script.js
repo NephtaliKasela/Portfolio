c8cfbb4c974bb4fbf037fa2df86b490ccb648418
Nephtali Kasela <nephtalikasela04@gmail.com>
2024-02-27 15:59:30 +0300

    Make small change on images

--- script.js
+++ script.js
@@ -55,3 +55,60 @@ const typed = new Typed('.multiple-text', {
     backDelay: 100,
     loop: true
 });
+
+
+
+
+
+// Récupérer le formulaire
+var form = document.querySelector('form');
+
+// Écouter l'événement de soumission du formulaire
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Empêcher le rechargement de la page
+
+  // Récupérer les valeurs des champs du formulaire
+  var fullName = form.querySelector('input[type="text"][placeholder="Full name"]').value;
+  var email = form.querySelector('input[type="email"]').value;
+  var mobileNumber = form.querySelector('input[type="number"]').value;
+  var subject = form.querySelector('input[type="text"][placeholder="Email Subject"]').value;
+  var message = form.querySelector('textarea').value;
+
+  // Créer un objet JSON avec les valeurs du formulaire
+  var formData = {
+    fullName: fullName,
+    email: email,
+    mobileNumber: mobileNumber,
+    subject: subject,
+    message: message
+  };
+
+  // Télécharger le fichier JSON existant
+  var jsonFile = 'data.json';
+
+  fetch(jsonFile)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Ajouter les nouvelles données au fichier JSON existant
+      data.push(formData);
+
+      // Convertir le tableau de données en JSON
+      var updatedJsonData = JSON.stringify(data);
+
+      // Télécharger le fichier JSON mis à jour
+      var downloadLink = document.createElement('a');
+      downloadLink.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(updatedJsonData);
+      downloadLink.download = 'data.json';
+      downloadLink.click();
+    })
+    .catch(function(error) {
+      console.error('Une erreur s\'est produite:', error);
+    });
+
+  // Réinitialiser le formulaire
+  form.reset();
+
+  // Afficher un message de confirmation ou rediriger l'utilisateur si nécessaire
+});
